136844b7d415aff41427240cc9d8033f1fc26022
JongMin7721 <wjm7721@naver.com>
2023-11-25 03:10:02 +0900

    sdffds

--- javascript3.js
+++ javascript3.js
@@ -1,8 +1,8 @@
 function purchase() {
     var p_c = document.querySelectorAll('.p_c');
-    var pc = document.getElementById(p_c[i].id);
 
     for (var i = 0; i < p_c.length; i++){
+        var pc = document.getElementById(p_c[i].id);
         console.log("id : ", p_c[i].id);
         console.log("idval : ", pc.id.replace(/\D/g,''));
         console.log("val : ", pc.value);
