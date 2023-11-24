1f8494d6ce127e1cd521d80eaa34d0f83469011f
JongMin7721 <wjm7721@naver.com>
2023-11-25 03:04:25 +0900

    fsdsf

--- javascript3.js
+++ javascript3.js
@@ -3,7 +3,7 @@ function purchase() {
 
     for (var i = 0; i < p_c.length; i++){
         console.log("id : ", p_c[i].id);
-        console.log("idval : ", document.getElementById(p_c[i]).id.replace(/\D/g,''));
+        console.log("idval : ", document.getElementById(p_c[i].id).id.replace(/\D/g,''));
         console.log("val : ", p_c[i].textContent);
     }
     
