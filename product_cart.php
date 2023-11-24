ef374e50625246f34fd01bce5e974bb380ed26f3
JongMin7721 <wjm7721@naver.com>
2023-11-24 22:28:24 +0900

    sdfjkl

---
+++ product_cart.php
@@ -0,0 +1,32 @@
+<?php
+
+include ("ipconfig.php");
+
+mysqli_select_db($conn, $dbname) or die('DB selection failed');
+
+$sql = "
+SELECT P.num_product, P.image_path, P.name_product, P.price
+From cart C JOIN product P
+on C.num_product = P.num_product;";
+
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+	while($row = $result->fetch_assoc()){
+		echo 
+        "<div class='product-container'>
+            <div class='product-card'>
+                <img src='", $row["image_path"], "' alt='상품 이미지'>
+                <h3>", $row["name_product"],"</h3>
+                <p>가격: ", $row["price"], "</p>
+                <label for='p", $row["num_product"],"> 갯수입력: </label>
+                <input type='number' id='p", $row["num_product"],"'>
+            </div>
+        </div>";
+	}
+}else{
+	echo "0 Results";
+}
+
+$conn->close();
+?>
